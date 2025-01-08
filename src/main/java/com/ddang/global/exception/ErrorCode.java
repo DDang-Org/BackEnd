aea5319e61442c43426d60fbfcabfa62f0f2cb72
MoonJaeGyeong <bblbblan69@gmail.com>
2025-01-08 15:27:01 +0900

    feat : #8 S3 Exception 추가

--- src/main/java/com/ddang/global/exception/ErrorCode.java
+++ src/main/java/com/ddang/global/exception/ErrorCode.java
@@ -2,10 +2,13 @@ package com.ddang.global.exception;
 
 public enum ErrorCode {
 
+    // S3
+    FILE_UPLOAD_FAIL("E_UPL", "BAD_REQUEST", "파일 업로드에 실패하였습니다."),
+    FILE_DOWNLOAD_FAIL("E_DOW", "BAD_REQUEST", "파일 다운로드에 실패하였습니다."),
+    FILE_TRANSACTION_FAIL("E_TRA", "BAD_REQUEST", "파일 변환에 실패하였습니다."),
 
     // 401 에러
     UNAUTHORIZED_ERROR("E_AUTH", "UNAUTHORIZED", "AccessToken is invalid"),
-
 
     // 500 에러
     INTERNAL_SERVER_ERROR("E_SYS", "INTERNAL_SERVER_ERROR", "알 수 없는 오류가 발생했습니다.");
