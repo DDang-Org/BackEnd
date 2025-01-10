214f24d35eaf9f68882bd19d8720023da59bf720
MoonJaeGyeong <bblbblan69@gmail.com>
2025-01-10 15:18:42 +0900

    test : #8 test 환경 통합

---
+++ src/test/java/com/ddang/ApiTestSupport.java
@@ -0,0 +1,25 @@
+package com.ddang;
+
+import com.ddang.dog.service.DogServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = {
+})
+@AutoConfigureMockMvc(addFilters = false)
+public abstract class ApiTestSupport {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @MockitoBean
+    protected DogServiceImpl dogServiceImpl;
+
+}
