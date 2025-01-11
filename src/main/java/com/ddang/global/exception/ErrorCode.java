b252d2c289179a41f8062b4c03716fcf905c95de
rudgns328 <rudgns328@gmail.com>
2025-01-11 17:01:15 +0900

    feat: Member 관련 공통 에러 처리 구현

--- src/main/java/com/ddang/global/exception/ErrorCode.java
+++ src/main/java/com/ddang/global/exception/ErrorCode.java
@@ -2,14 +2,18 @@ package com.ddang.global.exception;
 
 public enum ErrorCode {
 
-
     // 401 에러
-    UNAUTHORIZED_ERROR("E_AUTH", "UNAUTHORIZED", "AccessToken is invalid"),
-
+    UNAUTHORIZED_ATK_ERROR("E_AUTH", "UNAUTHORIZED", "AccessToken is invalid"),
+    UNAUTHORIZED_RTK_ERROR("E_AUTH", "UNAUTHORIZED", "RefreshToken is invalid"),
+    EXPIRED_TOKEN_ERROR("E_AUTH", "UNAUTHORIZED", "JWT 토큰이 만료되었습니다."),
+    UNSUPPORTED_TOKEN_ERROR("E_AUTH", "UNAUTHORIZED", "지원하지 않는 JWT 토큰입니다."),
+    EMPTY_TOKEN_ERROR("E_AUTH", "UNAUTHORIZED", "JWT 토큰이 비어 있습니다."),
 
     // 500 에러
-    INTERNAL_SERVER_ERROR("E_SYS", "INTERNAL_SERVER_ERROR", "알 수 없는 오류가 발생했습니다.");
+    INTERNAL_SERVER_ERROR("E_SYS", "INTERNAL_SERVER_ERROR", "알 수 없는 오류가 발생했습니다."),
 
+    // Member
+    MEMBER_NOT_FOUND("E_MEM", "NOT_FOUND", "Member not found");
 
     private final String code;
     private final String message;
---
+++ src/main/java/com/ddang/global/exception/MemberException.java
@@ -0,0 +1,6 @@
+package com.ddang.global.exception;
+
+public class MemberException extends CustomException{
+
+    public MemberException(ErrorCode errorCode) { super(errorCode);}
+}
