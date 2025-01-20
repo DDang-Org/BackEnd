b4d63bc542d48944c1a8c76737f81540caa4a2a8
MoonJaeGyeong <bblbblan69@gmail.com>
2025-01-20 15:12:43 +0900

    feat : #14 walk 및 walkLog Repository 추가

---
+++ src/main/java/com/ddang/walk/repository/WalkDogRepository.java
@@ -0,0 +1,52 @@
+package com.ddang.walk.repository;
+
+import com.ddang.walk.entity.Walk;
+import com.ddang.walk.entity.WalkDog;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface WalkDogRepository extends JpaRepository<WalkDog, Long> {
+
+   @Query("""
+    SELECT wd
+    FROM WalkDog wd
+    WHERE wd.dog.dogId = :dogId
+    AND wd.dog.isDeleted = 'FALSE'
+""")
+    List<WalkDog> findAllByDog_DogId(Long dogId);
+
+    @Query("""
+    SELECT wd 
+    FROM WalkDog wd 
+    WHERE YEAR(wd.createdAt) = :year 
+    AND wd.dog.dogId = :dogId""")
+    List<WalkDog> findWalkDogsByYearAndDogId(@Param("year") int year, @Param("dogId") Long dogId);
+
+    @Query("""
+    SELECT wd.walk
+    FROM WalkDog wd
+    WHERE wd.dog.dogId = :dogId
+""")
+    List<Walk> findWalksByDogIdFromMemberId(Long memberId, Long dogId);
+
+    @Query("""
+    SELECT wd.walk
+    FROM WalkDog wd
+    WHERE wd.dog.dogId = :dogId
+    And Month(wd.createdAt) = :month
+""")
+    List<Walk> findWalksByDogIdAndMonthFromMemberId(Long memberId, Long dogId, int month);
+
+    @Query("""
+    SELECT wd.walk 
+    FROM WalkDog wd 
+    JOIN FETCH wd.walk.member 
+    WHERE wd.dog.dogId = :dogId
+    AND YEAR(wd.createdAt) = :year
+""")
+    List<Walk> findWalksByDogId(@Param("dogId") Long dogId, @Param("year") int year);
+
+}
---
+++ src/main/java/com/ddang/walk/repository/WalkRepository.java
@@ -0,0 +1,25 @@
+package com.ddang.walk.repository;
+
+import com.ddang.member.entity.Member;
+import com.ddang.walk.entity.Walk;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface WalkRepository extends JpaRepository<Walk, Long> {
+
+    @EntityGraph(attributePaths = {"member"})
+    @Query(value = """
+        SELECT w
+        FROM Walk w 
+        WHERE w.member IN :members 
+        AND DATE(w.createdAt) = :date
+       """)
+    List<Walk> findAllByMembersAndDate(@Param("members") List<Member> members, @Param("date") LocalDate date);
+
+
+}
