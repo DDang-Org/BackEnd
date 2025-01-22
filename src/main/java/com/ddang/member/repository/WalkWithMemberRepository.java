ea22f4e981f96a01cfefd4c852921382b95ee499
rudgns328 <rudgns328@gmail.com>
2025-01-22 13:11:07 +0900

    feat: Member 산책 기록 repository 메소드 추가

---
+++ src/main/java/com/ddang/member/repository/WalkWithMemberRepository.java
@@ -0,0 +1,16 @@
+package com.ddang.member.repository;
+
+import com.ddang.member.entity.WalkWithMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface WalkWithMemberRepository extends JpaRepository<WalkWithMember, Long> {
+
+    @Query("""
+            SELECT COALESCE(COUNT(w), 0)
+            FROM WalkWithMember w
+            WHERE w.sender.memberId = :memberId AND w.isDeleted = 'FALSE'
+            """)
+    int countBySenderMemberId(@Param("memberId") Long memberId);
+}
