df3957963c4fceae751c296b07fc3fd1a4e0d2ca
rudgns328 <rudgns328@gmail.com>
2025-01-22 13:10:18 +0900

    feat: Member API 구현

---
+++ src/main/java/com/ddang/member/controller/request/UpdateRequest.java
@@ -0,0 +1,42 @@
+package com.ddang.member.controller.request;
+
+import com.ddang.global.entity.Gender;
+import com.ddang.member.entity.FamilyRole;
+import com.ddang.member.service.request.UpdateServiceRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Schema(description = "내 정보 수정 요청 데이터")
+public record UpdateRequest(
+
+        @NotBlank(message = "회원 이름을 입력해주세요.")
+        @Schema(description = "회원 이름", example = "홍길동")
+        String memberName,
+
+        @NotNull(message = "회원 성별을 입력해주세요.")
+        @Schema(description = "회원 성별", example = "MALE")
+        Gender memberGender,
+
+        @NotBlank(message = " 주소를 입력해주세요.")
+        @Schema(description = "회원 주소", example = "서울시 강남구")
+        String address,
+
+        @NotNull(message = "가족 역할을 입력해주세요.")
+        @Schema(description = "가족 역할", example = "FATHER")
+        FamilyRole familyRole,
+
+        @NotNull(message = "회원 프로필 이미지를 입력해주세요.")
+        @Schema(description = "회원 프로필 이미지", example = "https://example.com/profile.jpg")
+        int memberProfileImg
+) {
+    public UpdateServiceRequest toServiceRequest() {
+        return new UpdateServiceRequest(
+                memberName,
+                memberGender,
+                address,
+                familyRole,
+                memberProfileImg
+        );
+    }
+}
--- src/main/java/com/ddang/member/service/MemberServiceImpl.java
+++ src/main/java/com/ddang/member/service/MemberServiceImpl.java
@@ -3,11 +3,16 @@ package com.ddang.member.service;
 import com.ddang.global.exception.AuthenticationException;
 import com.ddang.global.exception.ErrorCode;
 import com.ddang.global.exception.MemberException;
+import com.ddang.member.controller.request.IsMatchedRequest;
+import com.ddang.member.entity.IsMatched;
 import com.ddang.member.entity.Member;
 import com.ddang.member.jwt.service.JwtService;
 import com.ddang.member.repository.MemberRepository;
+import com.ddang.member.repository.WalkWithMemberRepository;
 import com.ddang.member.service.request.JoinServiceRequest;
-import com.ddang.member.service.response.MemberResponse;
+import com.ddang.member.service.request.UpdateServiceRequest;
+import com.ddang.member.service.response.*;
+import com.ddang.walk.repository.WalkRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +27,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final WalkRepository walkRepository;
+    private final WalkWithMemberRepository walkWithMemberRepository;
     private final JwtService jwtService;
 
     @Override
@@ -83,5 +90,67 @@ public class MemberServiceImpl implements MemberService {
         }
 
         return "Success Logout";
+    }
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public MyPageResponse getMemberInfo(Long memberId) {
+        Member member = findMemberById(memberId);
+
+        return MyPageResponse.from(member);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public WalkInfoResponse getMemberWalkInfo(Long memberId) {
+        findMemberById(memberId);
+
+        int totalDistanceInMeters = walkRepository.findTotalDistanceByMemberId(memberId);
+        int countWalks = walkRepository.countWalksByMemberId(memberId);
+
+        double totalDistanceInKilometers = totalDistanceInMeters / 1000.0;
+        int countWalksWithMember = walkWithMemberRepository.countBySenderMemberId(memberId);
+
+        return WalkInfoResponse.from(totalDistanceInKilometers, countWalks, countWalksWithMember);
+    }
+
+    @Override
+    public IsMatchedResponse updateIsMatched(Long memberId, IsMatchedRequest isMatchedRequest) {
+        Member member = findMemberById(memberId);
+        IsMatched isMatched = validateIsMatched(isMatchedRequest.isMatched());
+
+        member.updateIsMatched(isMatched);
+        return IsMatchedResponse.from(member.getIsMatched());
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UpdateResponse getUpdateInfo(Long memberId) {
+        Member member = findMemberById(memberId);
+
+        return UpdateResponse.from(member);
+    }
+
+    @Override
+    public UpdateResponse updateMember(Long memberId, UpdateServiceRequest serviceRequest) {
+        Member member = findMemberById(memberId);
+
+        serviceRequest.toEntity(member);
+
+        return UpdateResponse.from(member);
+    }
+
+    private Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberException(ErrorCode.MEMBER_NOT_FOUND));
+    }
+
+    private IsMatched validateIsMatched(String isMatchedValue) {
+        try {
+            return IsMatched.valueOf(isMatchedValue);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            throw new MemberException(ErrorCode.INVALID_IS_MATCHED);
+        }
     }
 }
