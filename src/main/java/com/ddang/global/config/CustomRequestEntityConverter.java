e58fd1b2c143fa17b161f74d511316191ac80064
MoonJaeGyeong <bblbblan69@gmail.com>
2025-05-16 17:04:44 +0900

    fix : #73 key 파일 경로 변경

--- src/main/java/com/ddang/global/config/CustomRequestEntityConverter.java
+++ src/main/java/com/ddang/global/config/CustomRequestEntityConverter.java
@@ -68,7 +68,7 @@ public class CustomRequestEntityConverter implements Converter<OAuth2Authorizati
     }
 
     public PrivateKey getPrivateKey() throws IOException {
-        ClassPathResource resource = new ClassPathResource("key/" + APPLE_KEY_PATH);
+        ClassPathResource resource = new ClassPathResource("/src/main/resources/key/" + APPLE_KEY_PATH);
         // 배포시 jar 파일을 찾지 못함
         //String privateKey = new String(Files.readAllBytes(Paths.get(resource.getURI())));
 
