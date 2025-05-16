ad5d58f6b32c697a5cbd2ca823f1c585fe1453d9
MoonJaeGyeong <bblbblan69@gmail.com>
2025-05-16 16:23:42 +0900

    fix : #73 defaultConverter 초기화

--- src/main/java/com/ddang/global/config/CustomRequestEntityConverter.java
+++ src/main/java/com/ddang/global/config/CustomRequestEntityConverter.java
@@ -26,10 +26,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
-@RequiredArgsConstructor
 public class CustomRequestEntityConverter implements Converter<OAuth2AuthorizationCodeGrantRequest, RequestEntity<?>> {
 
     private OAuth2AuthorizationCodeGrantRequestEntityConverter defaultConverter;
+
+    public CustomRequestEntityConverter() {
+        this.defaultConverter = new OAuth2AuthorizationCodeGrantRequestEntityConverter();
+    }
+
     private final String APPLE_URL = "https://appleid.apple.com";
 
     @Value("${apple.key-name}")
